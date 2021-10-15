1f1f1169664029e4b9849fbe1dfc7b7fdf35cfa3
vikramadditya <86791011+vikramadditya@users.noreply.github.com>
2021-10-15 17:30:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload () {
+    
+}
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+function draw() { background(220); function draw() { background(0); let locX = mouseX - height / 2; let locY = mouseY - width / 2; ambientLight(50); directionalLight(255, 0, 0, 0.25, 0.25, 0); pointLight(0, 0, 255, locX, locY, 250); push(); translate(-width / 4, 0, 0); rotateZ(frameCount * 0.02); rotateX(frameCount * 0.02); specularMaterial(250); box(100, 100, 100); pop(); translate(width / 4, 0, 0); ambientMaterial(250); sphere(120, 64); } }
+function take_snapshot(){
+    save('myFilterImage.png')
+}
